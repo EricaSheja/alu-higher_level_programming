58e1315e1fb1f72a3d9b6b2aff18fe91d0101186
EricaSheja <e.sheja@alustudent.com>
2026-03-31 13:30:00 -0700

    last

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Same as 10-square.py but we override __str__
+# so print() shows [Square] instead of [Rectangle]
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        # Validate size using the inherited integer_validator method
+        self.integer_validator("size", size)
+
+        # Store size as a private attribute
+        self.__size = size
+
+        # Call Rectangle's __init__ with size for both width and height
+        super().__init__(size, size)
+
+    def area(self):
+        # Area = size x size
+        return self.__size * self.__size
+
+    def __str__(self):
+        # Override so it prints [Square] instead of [Rectangle]
+        return "[Square] {}/{}".format(self.__size, self.__size)
